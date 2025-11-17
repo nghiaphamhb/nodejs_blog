1b56f2a8107068e44f9a3681f921bcba36b87aab
second <phamdangtrungnghia@gmail.com>
2025-11-17 14:09:13 +0300

    refactor: create controller modules

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,14 @@
+class NewsController{
+
+    // [GET] /news
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [GET /news/:slug
+    show(req, res){
+        res.send('New details ' + req.params.slug);
+    }
+}
+
+export default new NewsController();
