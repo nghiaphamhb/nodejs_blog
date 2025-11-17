716901fd971449f7e7bb4e5e43563efd019d9f8d
second <phamdangtrungnghia@gmail.com>
2025-11-17 14:08:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,39 +3,26 @@ import morgan from 'morgan';
 import { engine } from 'express-handlebars';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import routes from './routes/routes.js';
 
 const app = express()
 const port = 3000
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'public')));  // public path for static files (_dirname/public/*)
 
 // HTTP logger
-// app.use(morgan('combined'))
+app.use(morgan('combined'))
 
-// Template engine
+// Template engine (handlerbars)
 app.engine('.hbs', engine({extname: '.hbs'}));  //handlebars
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resource', 'views'));
 
-app.get('/', (req, res) => {
-  res.render("home")
-})
-
-app.get('/news', (req, res) => {
-  res.render("news")
-})
-
-app.get('/search', (req, res) => {
-  console.log(req.query.q);
-  res.render("search");
-})
-
-app.post('/search', (req, res) => {
-  console.log(req.query.p);
-  res.render("search");
-})
+// routes init
+routes(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
