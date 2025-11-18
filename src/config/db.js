f2f8798f9b1836abcafc0c3e50b26bbebe41fe46
second <phamdangtrungnghia@gmail.com>
2025-11-18 13:18:06 +0300

    config: database

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'nodejs_blog',   
+  password: '', 
+  port: 5432,
+});
+
+export function query(text, params) {
+  return pool.query(text, params);
+}
