8e145d3ca62c9229fc27f3d23c02d79af2cc013f
second <phamdangtrungnghia@gmail.com>
2025-11-18 20:35:13 +0300

    feat: add render personal page's trash

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -1,14 +1,24 @@
 import ProductModel from '../models/ProductModel.js';
 
+// Controller
 class MeController {
-  // [GET] /stored/products
+  // [GET] /me/stored/products
   storedProducts(req, res, next) {
         ProductModel.findAll()
         .then(foods => res.render('me/store-products.hbs',
           {foods}
         ))
         .catch(next);
-    }
+  }
+
+  // [GET] /me/trash/products
+  trashProducts(req, res, next){
+    ProductModel.findAllWithDeleted()
+        .then(deletedFoods => res.render('me/trash-products.hbs',
+          {deletedFoods}
+        ))
+        .catch(next);
+  }
 }
 
 export default new MeController();
