abccf9b8d69a874893776cb41cbf44c507b61873
second <phamdangtrungnghia@gmail.com>
2025-11-18 20:36:30 +0300

    feat: add router to trash page

--- src/routes/me.js
+++ src/routes/me.js
@@ -4,5 +4,6 @@ import meController from '../app/controllers/MeController.js';
 const meRouter = express.Router();
 
 meRouter.get('/stored/products', meController.storedProducts);
+meRouter.get('/trash/products', meController.trashProducts);
 
 export default meRouter;
